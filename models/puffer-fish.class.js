814053073d914237f7cee266d95ecab4c8a06b39
GeorgiosKoinaris <g.koinaris@gmail.com>
2022-09-14 12:34:52 +0200

    added backgrounds

--- models/puffer-fish.class.js
+++ models/puffer-fish.class.js
@@ -5,5 +5,6 @@ class PufferFish extends MovableObject {
         super().loadImage('img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png');
 
         this.x = 400 + Math.random() * 500; //Zahl zwischen 200 und 700
+        this.y = 50 + Math.random() * 200; //Zahl zwischen 50 und 200
     }
 }
--- models/world.class.js
+++ models/world.class.js
@@ -9,7 +9,9 @@ class World {
         new Light()
     ];
     backgroundObjects = [
-        new BackgroundObject('img/3. Background/Legacy/Layers/3.Fondo 1/L1.png', 0)
+        new BackgroundObject('img/3. Background/Legacy/Layers/4.Fondo 2/L1.png', 0),
+        new BackgroundObject('img/3. Background/Legacy/Layers/3.Fondo 1/L1.png', 0),
+        new BackgroundObject('img/3. Background/Legacy/Layers/2. Floor/L1.png', 0)
     ];
     canvas;
     ctx;
@@ -24,10 +26,10 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+        this.addObjectsToMap(this.backgroundObjects);
         this.addToMap(this.character);
         this.addObjectsToMap(this.enemies);
         this.addObjectsToMap(this.lights);
-        this.addObjectsToMap(this.backgroundObjects);
 
         //draw() wird immer wieder aufgerufen
         let self = this;
