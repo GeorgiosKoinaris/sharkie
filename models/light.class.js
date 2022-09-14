295fd95c212ebdcf4874cc43c25dbc651f784fb6
GeorgiosKoinaris <g.koinaris@gmail.com>
2022-09-14 12:01:36 +0200

    added first background + optimized functions

---
+++ models/light.class.js
@@ -0,0 +1,11 @@
+class Light extends MovableObject {
+    x = 120;
+    y = 0;
+    width = 600;
+    height = 550;
+
+    constructor() {
+        super().loadImage('img/3. Background/Legacy/Layers/1. Light/1.png');
+
+    }
+}
--- models/world.class.js
+++ models/world.class.js
@@ -5,16 +5,44 @@ class World {
         new PufferFish(),
         new PufferFish(),
     ];
+    lights = [
+        new Light()
+    ];
+    backgroundObjects = [
+        new BackgroundObject('img/3. Background/Legacy/Layers/3.Fondo 1/L1.png', 0)
+    ];
+    canvas;
     ctx;
 
     constructor(canvas) {
         this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
         this.draw();
     }
 
 
     draw() {
-        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height)
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+        this.addToMap(this.character);
+        this.addObjectsToMap(this.enemies);
+        this.addObjectsToMap(this.lights);
+        this.addObjectsToMap(this.backgroundObjects);
+
+        //draw() wird immer wieder aufgerufen
+        let self = this;
+        requestAnimationFrame(function() {
+            self.draw();
+        })
+    }
+
+    addObjectsToMap(objects) {
+        objects.forEach(o => {
+            this.addToMap(o);
+        });
+    }
+
+    addToMap(mo) {
+        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
     }
 }
