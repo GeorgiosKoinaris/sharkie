cf0f559cdb9a35927ecc47c1f117fffe35822ce3
GeorgiosKoinaris <g.koinaris@gmail.com>
2022-10-05 11:53:56 +0200

    added new classes DrawableObject + StatusBar

---
+++ models/drawable-object.class.js
@@ -0,0 +1,33 @@
+class DrawableObject {
+    x = 120;
+    y = 250;
+    height = 150;
+    width = 150;
+    img;
+    imageCache = {};
+    currentImage = 0;
+
+
+    loadImage(path) {
+        this.img = new Image();
+        this.img.src = path;
+    }
+
+    /**
+     * 
+     * @param {Array} arr // ['img/image1.png', 'img/image2.png', ...]
+     */
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+        });
+    }
+
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+
+
+}
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,37 +1,10 @@
-class MovableObject {
-    x = 120;
-    y = 250;
-    img;
-    height = 150;
-    width = 150;
-    imageCache = {};
-    currentImage = 0;
+class MovableObject extends DrawableObject {
+
     speed = 0.15;
     otherDirection = false;
     energy = 100;
     lastHit = 0;
 
-
-    loadImage(path) {
-        this.img = new Image();
-        this.img.src = path;
-    }
-
-    /**
-     * 
-     * @param {Array} arr // ['img/image1.png', 'img/image2.png', ...]
-     */
-    loadImages(arr) {
-        arr.forEach((path) => {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-        });
-    }
-
-    draw(ctx) {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-    }
 
     drawFrame(ctx) {
         if (this instanceof PufferFish) {
