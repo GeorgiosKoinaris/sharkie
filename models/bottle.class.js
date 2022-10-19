abaa65f99d10595affe8029ed636148d4bb777b5
GeorgiosKoinaris <g.koinaris@gmail.com>
2022-10-19 10:08:50 +0200

    added animation for coin and poison

--- models/bottle.class.js
+++ models/bottle.class.js
@@ -4,9 +4,28 @@ class Bottle extends MovableObject {
     height = 80;
     width = 60;
 
+    IMAGES_POISON = [
+        'img/4. Marcadores/Posión/Animada/1.png',
+        'img/4. Marcadores/Posión/Animada/2.png',
+        'img/4. Marcadores/Posión/Animada/3.png',
+        'img/4. Marcadores/Posión/Animada/4.png',
+        'img/4. Marcadores/Posión/Animada/5.png',
+        'img/4. Marcadores/Posión/Animada/6.png',
+        'img/4. Marcadores/Posión/Animada/7.png',
+        'img/4. Marcadores/Posión/Animada/8.png'
+    ]
+
     constructor() {
-        super().loadImage('img/4. Marcadores/Posión/Light - Left.png');
+        super().loadImage('img/4. Marcadores/Posión/Animada/1.png');
+        this.loadImages(this.IMAGES_POISON);
         this.x = 200 + Math.random() * 2000;
-        this.y = 355;
+        this.y = 340;
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            this.playAnimation(this.IMAGES_POISON);
+        }, 150);
     }
 }
--- models/coin.class.js
+++ models/coin.class.js
@@ -4,9 +4,24 @@ class Coin extends MovableObject {
     height = 55;
     width = 50;
 
+    IMAGES_COIN = [
+        'img/4. Marcadores/1. Coins/1.png',
+        'img/4. Marcadores/1. Coins/2.png',
+        'img/4. Marcadores/1. Coins/3.png',
+        'img/4. Marcadores/1. Coins/4.png'
+    ]
+
     constructor() {
         super().loadImage('img/4. Marcadores/1. Coins/1.png');
+        this.loadImages(this.IMAGES_COIN);
         this.x = 200 + Math.random() * 2000;
         this.y = 355;
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            this.playAnimation(this.IMAGES_COIN);
+        }, 300);
     }
 }
